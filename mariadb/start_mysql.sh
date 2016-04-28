e0146f5d1592cff96ed7082efac99d0ca72c2394
Tiago Caxias <tcx.taliesin@gmail.com>
2016-04-28 17:31:00 +0100

    Changed redis user to "redis"

--- mariadb/start_mysql.sh
+++ mariadb/start_mysql.sh
@@ -8,4 +8,8 @@ chown mysql.mysql -R \
     /var/log/mysql \
     /run/mysqld
 
+[ "$COMPRESS_RSYNC" == "yes" ] && \
+    sed -i -e 's|WHOLE_FILE_OPT="|WHOLE_FILE_OPT="--compress |g' \
+    /usr/bin/wsrep_sst_rsync
+
 exec mysqld_safe $@
