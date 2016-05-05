e3d09d719c63b7847db035aceeb33a64e0f26389
Tiago Caxias <tcx.taliesin@gmail.com>
2016-05-05 15:57:22 +0100

    Added a presto CT

--- mariadb.debian/start_mysql.sh
+++ mariadb.debian/start_mysql.sh
@@ -3,9 +3,16 @@
 [ -d /var/lib/mysql/mysql ] || \
     mysql_install_db
 
+mkdir -p \
+    /var/lib/mysql \
+    /var/log/mysql \
+    /var/run/mysqld/tmp \
+    /run/mysqld
+
 chown mysql.mysql -R \
     /var/lib/mysql \
     /var/log/mysql \
+    /var/run/mysqld \
     /run/mysqld
 
 exec mysqld_safe --plugin-load=server_audit.so $@
