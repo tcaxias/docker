cc57afae488ebe1cee7d29d3f2300228830228aa
Tiago Caxias <tcx.taliesin@gmail.com>
2016-06-21 13:37:38 +0100

    Added readme files for redis and mongodb

---
+++ mongodb/README.md
@@ -0,0 +1,13 @@
+# A simple container for MongoDB
+
+Please find orchestrator at https://github.com/mongodb/mongo
+
+Docs at https://docs.mongodb.com/
+
+Please define PASSWD as an env variable if you wish authentication.
+
+You may run this using:
+
+    docker run -d --net host -e tcx/mongodb
+    or
+    docker run -d --net host -e PASSWD=MySuPeRdUpErPaSsW0rD tcx/mongodb
---
+++ mongodb/start_mongodb.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ "_$PASS" != "_" ] ;
+then
+mongod --fork --syslog && \
+    until { echo 'show dbs' | mongo; } ; do sleep 1 ; done && \
+    echo "use admin ; db.createUser({" \
+        "user:\"admin\",pwd:\"$PASSWD\"," \
+        "roles:[{role:\"userAdminAnyDatabase\",db:\"admin\"}]}) ;" \
+    | tr ';' '\n' \
+    | mongo \
+    && mongod --shutdown
+fi
+
+exec mongod --auth --nojournal --httpinterface --rest
