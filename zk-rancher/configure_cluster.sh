d70f410a97ca439a29c05fcab67e51669b850406
Tiago Caxias <tcx.taliesin@gmail.com>
2016-09-13 11:23:51 +0100

    Added a check to the unicity of the IDs

--- zk-rancher/configure_cluster.sh
+++ zk-rancher/configure_cluster.sh
@@ -4,10 +4,20 @@
 # and https://github.com/lukeolbrish/examples/tree/master/zookeeper/five-server-docker
 # Please respect the licenses on those repos
 
-sleep 10
-until grep -q '169.254.169.250' /etc/resolv.conf; do sleep 1; done
+until grep -q '169.254.169.250' /etc/resolv.conf; do
+    sleep 1
+done
 
 METADATA="http://169.254.169.250/latest"
+
+IDS='x'
+UNQ=''
+until [ $IDS = $UNQ ]; do
+    IDS=$(curl --header 'Accept: application/json' "$METADATA/self/service/containers" | jq  -r '.[] | .service_index ' -c | sort|tr '\n' ' ')
+    UNQ=$(curl --header 'Accept: application/json' "$METADATA/self/service/containers" | jq  -r '.[] | .service_index ' -c | sort|uniq|tr '\n' ' ')
+    sleep 1
+done
+
 MYID=$(curl --header 'Accept: application/json' "$METADATA/self/container/service_index"|jq -r .)
 IPS=$(curl --header 'Accept: application/json' "$METADATA/self/service/containers"|jq  -r '.[] | .ports[0]'|cut -f1 -d':')
 
