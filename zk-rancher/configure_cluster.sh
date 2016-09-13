2b9bb12dd183997d69884f79a140e182f696e5dc
Tiago Caxias <tcx.taliesin@gmail.com>
2016-09-13 15:18:33 +0100

    Moved the cluster IP to internal addresses

--- zk-rancher/configure_cluster.sh
+++ zk-rancher/configure_cluster.sh
@@ -4,6 +4,8 @@
 # and https://github.com/lukeolbrish/examples/tree/master/zookeeper/five-server-docker
 # Please respect the licenses on those repos
 
+cp conf/zoo_sample.cfg conf/zoo.cfg
+
 until grep -q '169.254.169.250' /etc/resolv.conf; do
     sleep 1
 done
@@ -16,8 +18,8 @@ echo $MYID | tee /var/zookeeper/myid > /tmp/zookeeper/myid
 
 curl --header 'Accept: application/json' \
     "$METADATA/self/service/containers" | \
-    jq  -r '.[] | if .state=="stopped" then  "" else [ .service_index , .ports[0] ] end' -c | \
-    sed -r -e 's#\["([^"]+)","([^:]+).*#server.\1=\2:2888:3888#g' >> \
+    jq  -r '.[] | if .state=="stopped" then  "" else [ .service_index , .primary_ip ] end' -c | \
+    sed -r -e 's#\["([^"]+)","([^"]+)".*#server.\1=\2:2888:3888#g' >> \
     /opt/zookeeper/conf/zoo.cfg
 
 exec /opt/zookeeper/bin/zkServer.sh start-foreground
