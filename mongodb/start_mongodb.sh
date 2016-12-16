ee8a54645e3e5b9dc56e96183087fad7f4161798
Tiago Caxias <tcx.taliesin@gmail.com>
2016-12-16 11:30:36 +0000

    Made mongodb bootstrap happen only when DB is not empty

--- mongodb/start_mongodb.sh
+++ mongodb/start_mongodb.sh
@@ -2,26 +2,30 @@
 
 AUTH=""
 SETPASS="show dbs"
-if [ "_$PASSWD" != "_" ] ;
+
+if [ ! -e "/data/db/storage.bson" ];
 then
-    SETPASS="use admin ; db.createUser({user:\"admin\",pwd:\"$PASSWD\",roles:[\"root\"]}) ;"
+    if [ "_$PASSWD" != "_" ] ;
+    then
+        SETPASS="use admin ; db.createUser({user:\"admin\",pwd:\"$PASSWD\",roles:[\"root\"]}) ;"
+    fi
+
+    mongod --storageEngine $ENGINE --fork --syslog $MAINT_PORT && \
+        until { echo 'show dbs' | mongo $MAINT_PORT; } ; do sleep 1 ; done && \
+        echo $SETPASS | tr ';' '\n' | mongo $MAINT_PORT && \
+        { [ -d /app/include ] && for i in /app/include/*.sh ; do . $i ; done || : ;} && \
+        mongod --shutdown && \
+        AUTH="--auth"
+
+    while pidof mongod; do sleep 1; done
+
+    mongod --storageEngine $ENGINE $MONGO_OPTS --fork --syslog && \
+        sleep 1 && \
+        until { echo 'rs.initiate({_id:"x",members:[{_id:0, host:"localhost"}]})' \
+                    | mongo local; } ; do sleep 1 ; done && \
+        mongod --shutdown
+
+    while pidof mongod; do sleep 1; done
 fi
-
-mongod --storageEngine $ENGINE --fork --syslog $MAINT_PORT && \
-    until { echo 'show dbs' | mongo $MAINT_PORT; } ; do sleep 1 ; done && \
-    echo $SETPASS | tr ';' '\n' | mongo $MAINT_PORT && \
-    { [ -d /app/include ] && for i in /app/include/*.sh ; do . $i ; done || : ;} && \
-    mongod --shutdown && \
-    AUTH="--auth"
-
-while pidof mongod; do sleep 1; done
-
-mongod --storageEngine $ENGINE $MONGO_OPTS --fork --syslog && \
-    sleep 1 && \
-    until { echo 'rs.initiate({_id:"x",members:[{_id:0, host:"localhost"}]})' \
-                | mongo local; } ; do sleep 1 ; done && \
-    mongod --shutdown
-
-while pidof mongod; do sleep 1; done
 
 exec mongod --storageEngine $ENGINE $AUTH $MONGO_OPTS
