36d4bc4e51b9a12730e3059ba2501b11c359c9bb
Tiago Caxias <tcx.taliesin@gmail.com>
2017-02-24 10:35:24 +0000

    Added a container for riak with cs, kv and stanshion all in the same container

---
+++ riak/s2/start_stanchion.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sed -i /etc/stanchion/stanchion.conf \
+    -e "s#listener = 127.0.0.1:8085#listener = 0.0.0.0:8085#" \
+    -e "s#log.console = file#log.console = console#"
+
+export KEY=$(curl -q 127.0.0.1:8098/buckets/admin/keys/key | \
+    openssl enc -a -d -aes-256-cbc -nosalt -pass pass:$PASSWD -A)
+export SECRET=$(curl -q 127.0.0.1:8098/buckets/admin/keys/secret | \
+    openssl enc -a -d -aes-256-cbc -nosalt -pass pass:$PASSWD -A)
+
+sed -i '/admin\./d' /etc/stanchion/stanchion.conf
+echo "admin.key = $KEY" >> /etc/stanchion/stanchion.conf
+echo "admin.secret = $SECRET" >> /etc/stanchion/stanchion.conf
+
+exec stanchion console
